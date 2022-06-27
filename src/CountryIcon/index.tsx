3851922fb961aedf778bd936ae44e3f88a04bcd7
Kondrat Shmoylov <remhag@gmail.com>
2022-06-27 15:46:47 +0200

    chore: custom prefix

--- src/CountryIcon/index.tsx
+++ src/CountryIcon/index.tsx
@@ -1,8 +1,12 @@
 import { styled } from '@material-ui/core'
 import Icon from '@material-ui/core/Icon';
 import { FC } from 'react'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 export const testId = 'country-icon'
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'jss-react-country-icon',
+});
 
 const StyledImg = styled('img')({
   verticalAlign: 'middle',
@@ -25,11 +29,13 @@ export const CountryIcon: FC<CountryIconProps> = ({ countryCode, className }) =>
   const isQuadratic = countryCode === 'CH' || countryCode === 'VA'
   const svgURL = `//unpkg.com/react-country-icon@${process.env.REACT_PACKAGE_VERSION}/dist/icons/${isQuadratic ? 'flags_1x1' : 'flags_4x3'}/${countryCode.toLowerCase()}.svg`
   return (
-    <Icon>
-      {/* @ts-ignore */}
-      {!isQuadratic && <StyledImg className={className} src={svgURL} alt=""/>}
-      {isQuadratic && <StyledImgQuadractic className={className} src={svgURL} alt=""/>}
-    </Icon>
+    <StylesProvider generateClassName={generateClassName}>
+      <Icon>
+        {/* @ts-ignore */}
+        {!isQuadratic && <StyledImg className={className} src={svgURL} alt=""/>}
+        {isQuadratic && <StyledImgQuadractic className={className} src={svgURL} alt=""/>}
+      </Icon>
+    </StylesProvider>
   )
 
 
