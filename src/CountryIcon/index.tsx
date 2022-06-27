a1db876429796e1932ded1d17a0ff12b34b37f59
Kondrat Shmoylov <remhag@gmail.com>
2022-06-27 09:56:47 +0200

    avoid style overlap with css modules

--- src/CountryIcon/index.tsx
+++ src/CountryIcon/index.tsx
@@ -1,38 +1,34 @@
-import { makeStyles } from '@material-ui/core'
+import { styled } from '@material-ui/core'
 import Icon from '@material-ui/core/Icon';
 import { FC } from 'react'
-import classnames from 'classnames'
 
 export const testId = 'country-icon'
 
-const useStyles = makeStyles({
-  imageIcon: {
-    verticalAlign: 'middle',
-    width: '24px',
-    height: '16px',
-  },
-  imageIconQuadraric: {
-    width: '16px',
-    height: '16px',
-  },
+const StyledImg = styled('img')({
+  verticalAlign: 'middle',
+  width: '24px',
+  height: '16px',
+})
+
+const StyledImgQuadractic = styled('img')({
+  verticalAlign: 'middle',
+  width: '24px',
+  height: '16px',
 })
 
 interface CountryIconProps {
   countryCode: string
   className?: string
-  svgProps?: React.SVGProps<SVGSVGElement>
 }
 
-export const CountryIcon: FC<CountryIconProps> = ({ countryCode, className, svgProps }) => {
+export const CountryIcon: FC<CountryIconProps> = ({ countryCode, className }) => {
   const isQuadratic = countryCode === 'CH' || countryCode === 'VA'
-  const classes = useStyles()
-
+  const svgURL = `//unpkg.com/react-country-icon@${process.env.REACT_PACKAGE_VERSION}/dist/icons/${isQuadratic ? 'flags_1x1' : 'flags_4x3'}/${countryCode.toLowerCase()}.svg`
   return (
     <Icon>
       {/* @ts-ignore */}
-      <img alt="" className={classnames(classes.imageIcon, className, {
-        [classes.imageIconQuadraric]: isQuadratic,
-      })} src={`//unpkg.com/react-country-icon@${process.env.REACT_PACKAGE_VERSION}/dist/icons/${isQuadratic ? 'flags_1x1' : 'flags_4x3'}/${countryCode.toLowerCase()}.svg`}/>
+      {!isQuadratic && <StyledImg className={className} src={svgURL} alt=""/>}
+      {isQuadratic && <StyledImgQuadractic className={className} src={svgURL} alt=""/>}
     </Icon>
   )
 
